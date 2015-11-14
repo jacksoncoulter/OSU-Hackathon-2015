dae63bf94f7727a3f31d300ba0d9dd9d9f24a057
Drew Brown <brownaj3@miamioh.edu>
2015-11-14 15:04:36 -0500

    Actual upload of Skill
    Actually has shit in it

--- Skill.java
+++ Skill.java
@@ -1,4 +1,60 @@
 
 public class Skill {
+protected String name;
+protected int baseValue;
+protected int modifier;
+
+public Skill(){
+	
+}
+
+public Skill(String s, int base, int mod){
+	name = s;
+	baseValue = base;
+	modifier = mod;
+}
+
+public String getName(){
+	return name;
+}
+
+public int getBaseValue(){
+	return baseValue;
+}
+
+public int getModifier(){
+	return modifier;
+}
+
+public boolean setBaseValue(int i){
+	baseValue =  i;
+	return true;
+}
+
+public boolean setModifier(int i){
+	modifier = i;
+	return true;
+}
+
+public boolean incrementBaseValue(){
+	baseValue++;
+	return true;
+}
+
+public boolean incrementModifer(){
+	modifier++;
+	return true;
+}
+
+//returns the sum of baseValue and modifier. This is used for actual math stuff (damage dealt, health remaining, etc)
+public int getValue(){
+	return baseValue + modifier;
+}
+
+
+
+
+
+
 
 }
