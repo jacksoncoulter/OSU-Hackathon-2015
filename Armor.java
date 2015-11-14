7ac0bfeac555171b3678abbd720ed96cf3459d19
zachM94 <zach.mcquigg@gmail.com>
2015-11-14 17:43:34 -0500

    Added constructors

--- Armor.java
+++ Armor.java
@@ -1,47 +1,36 @@
-package game;
-
-public class Armor {
+public class Armor extends WorldObject {
+	//collidable = false;
+	//location = ?;
+	//interactable = true;
+	//size = ?;
 	private String name;
 	private double weight;
 	private int level;
 	private int defense;
 	
-	public Armor(){
-		
-		
-	}
 	
-	public Armor(){
-		
-		
-	}
-	
-	public boolean setName(String name){
+	public Armor(String name, double weight, int level, int defense){
 		this.name = name;
-		return true;
-		
-	}
-	
-	public boolean setClass(){
-		
-		
-	}
-	
-	public boolean setDefense(int defense){
-		this.defense = defense;
-		return true;
-	}
-	
-	public boolean setWeight(double weight){
 		this.weight = weight;
-		return true;
-	}
-	
-	public boolean setLevel(int level){
 		this.level = level;
-		return true;
+		this.defense = defense;
+		
 	}
 	
+	public Armor(){
+		Armor("Nakey", 1.0, 1, 10);
+		
+	}
+	
+	public void draw(){
+		
+		
+	}
+	
+	public void interact(){
+		
+	}
+		
 	public String getName(){
 		return this.name;
 		
@@ -62,6 +51,27 @@ public class Armor {
 		return this.level;
 		
 	}
+	
+	public boolean setName(String name){
+		this.name = name;
+		return true;
+	}
+	
+	public boolean setDefense(int defense){
+		this.defense = defense;
+		return true;
+	}
+	
+	public boolean setWeight(double weight){
+		this.weight = weight;
+		return true;
+	}
+	
+	public boolean setLevel(int level){
+		this.level = level;
+		return true;
+	}
+
 }
 
 
