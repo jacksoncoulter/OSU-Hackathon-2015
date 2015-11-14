2ad1ee3bc93dd202f84cb3648a8af5c64546846f
Kyle <sandstkj@miamioh.edu>
2015-11-14 16:48:08 -0500

    Added ItemSlot and Inventory Functionality
    Added all methods to ItemSlot class, Inventory updated because Eclipse
    was not recognizing it as a class.

--- ItemSlot.java
+++ ItemSlot.java
@@ -1,4 +1,57 @@
+import "Item.java"
+import java.util.*;
+
 public class ItemSlot{
 	
+	private Item storedItem;
+	private int count;
+	private boolean stackable;
+	private int maxStackSize;
+	private int DEFAULT_MAX_STACK_SIZE;
+	
+	//Constructors
+	public ItemSlot(){
+		ItemSlot(null, false);
+	}
+	
+	public ItemSlot(Item i, boolean stackable){
+		ItemSlot (i, stackable, 1)
+	}
+	
+	public ItemSlot (Item i, boolean stackable, int count){
+		storedItem = i;
+		this.stackable = stackable;
+		this.count = count;
+		maxStackSize = DEFAULT_MAX_STACK_SIZE;
+	}
+
+	public boolean incrementStack(){
+		if (stackable && count < maxStackSize){
+			count++;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	//Getters and Setters
+	public String getItemName(){
+		return storedItem.getName();
+	}
+	
+	public int getCount(){
+		return count;
+	}
+	
+	public boolean getStackable(){
+		return stackable;
+	}
+	
+	public double getWeight(){
+		return storedItem.weight * count;
+	}
+	
+	public boolean setObject(Item i){
+		storedItem = i;
+	}
 	
 }
---
+++ src/Inventory.java
@@ -0,0 +1,18 @@
+import "ItemSlot.java";
+
+public class Inventory{
+	
+	double maxWeight;
+	ArrayList<ItemSlot> slots;
+	boolean maxCapacity;
+	double DEFAULT_MAX_WEIGHT = 50;
+
+	public Inventory(){
+		
+	}
+	
+	public Inventory(){
+		
+	}
+	
+}
