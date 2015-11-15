f5d6de5614ca683d4e2a69e2e3e636b25c91b984
ethan <lapeed@miamioh.edu>
2015-11-14 20:22:40 -0500

    added collision detection for the player

--- osu/src/edu/miami/game/player/Player.java
+++ osu/src/edu/miami/game/player/Player.java
@@ -6,12 +6,23 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.tiled.TiledMap;
+
+import edu.miami.game.map.Tile;
 
 public class Player {
 	private float x;
 	private float y;
+	private float drawX;
+	private float drawY;
 	private float width;
 	private float height;
+	private float hitboxMarginX;
+	private float hitboxMarginY;
+	private float speed;
+	
+	private int mapDrawX;
+	private int mapDrawY;
 	
 	private int leftKey;
 	private int rightKey;
@@ -32,8 +43,13 @@ public class Player {
 		upKey = Input.KEY_W;
 		downKey = Input.KEY_S;
 		
-		width = 20;
-		height = 20;
+		width = image.getWidth();
+		height = image.getHeight();
+		
+		hitboxMarginX = 5;
+		hitboxMarginY = 5;
+		
+		speed = 4;
 		
 		// get the center of the game board
 		float centerX = con.getWidth() / 2;
@@ -41,8 +57,35 @@ public class Player {
 		
 		// the center of the character will be in the center of the screen
 		// not the top left of the character
-		x = centerX - (width / 2);
-		y = centerY - (height / 2);
+		drawX = centerX - (width / 2);
+		drawY = centerY - (height / 2);
+		
+		// the map will be drawn at location (0,0) when the game starts
+		mapDrawX = 0;
+		mapDrawY = 0;
+		
+		setPosition(drawX, drawY);
+	}
+	
+	public void setPosition(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public float getX() {
+		return x;
+	}
+	
+	public float getY() {
+		return y;
+	}
+	
+	public int getMapX() {
+		return mapDrawX;
+	}
+	
+	public int getMapY() {
+		return mapDrawY;
 	}
 	
 	public int getLeftKey() {
@@ -61,11 +104,105 @@ public class Player {
 		return downKey;
 	}
 	
-	public void render(GameContainer con, StateBasedGame game, Graphics g) throws SlickException {
-		g.drawImage(image, x, y);
+	private void processMovement(Input input, TiledMap map) {
+		if (input.isKeyDown(leftKey)) {
+			moveLeft(map);
+		} else if (input.isKeyDown(rightKey)) {
+			moveRight(map);
+		}
+		
+		if (input.isKeyDown(upKey)) {
+			moveUp(map);
+		} else if (input.isKeyDown(downKey)) {
+			moveDown(map);
+		}
 	}
 	
-	public void update(GameContainer con, StateBasedGame game, int delta) throws SlickException {
+	public void moveLeft(TiledMap map) {
+		for (int i = 0; i < speed; i++) {
+			float newX = (x + hitboxMarginX) - 1;
 
+			int tileX = (int) newX / 20;
+			int tileTopY = (int) (y + hitboxMarginY) / 20;
+			int tileBottomY = (int) (y + height - hitboxMarginY) / 20;
+			
+			int tileTopId = map.getTileId(tileX, tileTopY, 1);
+			int tileBottomId = map.getTileId(tileX, tileBottomY, 1);
+			
+			if (Tile.tileIsSolid(tileTopId) || Tile.tileIsSolid(tileBottomId)) {
+				return;
+			}
+			
+			x--;
+			mapDrawX++;
+		}
+	}
+	
+	public void moveRight(TiledMap map) {
+		for (int i = 0; i < speed; i++) {
+			float newX = (x + width - hitboxMarginX) + 1;
+	
+			int tileX = (int) newX  / 20;
+			int tileTopY = (int) (y + hitboxMarginY) / 20;
+			int tileBottomY = (int) (y + height - hitboxMarginY) / 20;
+			
+			int tileTopId = map.getTileId(tileX, tileTopY, 1);
+			int tileBottomId = map.getTileId(tileX, tileBottomY, 1);
+			
+			if (Tile.tileIsSolid(tileTopId) || Tile.tileIsSolid(tileBottomId)) {
+				return;
+			}
+			
+			x++;
+			mapDrawX--;
+		}
+	}
+	
+	public void moveUp(TiledMap map) {
+		for (int i = 0; i < speed; i++) {
+			float newY = (y + hitboxMarginY) - 1;
+	
+			int tileY = (int) newY / 20;
+			int tileLeftX = (int) (x + hitboxMarginX) / 20;
+			int tileRightX = (int) (x + width - hitboxMarginX) / 20;
+			
+			int tileLeftId = map.getTileId(tileLeftX, tileY, 1);
+			int tileRightId = map.getTileId(tileRightX, tileY, 1);
+			
+			if (Tile.tileIsSolid(tileLeftId) || Tile.tileIsSolid(tileRightId)) {
+				return;
+			}
+			
+			y--;
+			mapDrawY++;
+		}
+	}
+	
+	public void moveDown(TiledMap map) {
+		for (int i = 0; i < speed; i++) {
+			float newY = (y + height - hitboxMarginY) + 1;
+	
+			int tileY = (int) newY / 20;
+			int tileLeftX = (int) (x + hitboxMarginX) / 20;
+			int tileRightX = (int) (x + width - hitboxMarginX) / 20;
+			
+			int tileLeftId = map.getTileId(tileLeftX, tileY, 1);
+			int tileRightId = map.getTileId(tileRightX, tileY, 1);
+			
+			if (Tile.tileIsSolid(tileLeftId) || Tile.tileIsSolid(tileRightId)) {
+				return;
+			}
+			
+			y++;
+			mapDrawY--;
+		}
+	}
+	
+	public void render(GameContainer con, StateBasedGame game, Graphics g) throws SlickException {
+		g.drawImage(image, drawX, drawY);
+	}
+	
+	public void update(GameContainer con, StateBasedGame game, int delta, TiledMap map) throws SlickException {
+		processMovement(con.getInput(), map);
 	}
 }
--- osu/src/edu/miami/game/states/GamePlayState.java
+++ osu/src/edu/miami/game/states/GamePlayState.java
@@ -2,82 +2,42 @@ package edu.miami.game.states;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.tiled.TiledMap;
 
+import edu.miami.game.map.Tile;
 import edu.miami.game.player.Player;
 
 public class GamePlayState extends BasicGameState {
-	private int mapX;
-	private int mapY;
-	
 	private TiledMap map;
 	
 	private Player player;
 	
-	private void processMovement(Input input) {
-		if (input.isKeyDown(player.getLeftKey())) {
-			playerMoveLeft();
-		} else if (input.isKeyDown(player.getRightKey())) {
-			playerMoveRight();
-		}
-		
-		if (input.isKeyDown(player.getUpKey())) {
-			playerMoveUp();
-		} else if (input.isKeyDown(player.getDownKey())) {
-			playerMoveDown();
-		}
-	}
-	
-	private void playerMoveLeft() {
-		mapX++;
-	}
-	
-	private void playerMoveRight() {
-		mapX--;
-	}
-	
-	private void playerMoveUp() {
-		mapY++;
-	}
-	
-	private void playerMoveDown() {
-		mapY--;
-	}
-	
-	private void resetMap() {
-		mapX = 0;
-		mapY = 0;
-	}
-
 	@Override
 	public void init(GameContainer con, StateBasedGame game) throws SlickException {
-		resetMap();
-		
 		map = new TiledMap("res/map.tmx");
+		
+		Tile.parseMap(map);
 		
 		player = new Player(con);
 	}
 
 	@Override
 	public void render(GameContainer con, StateBasedGame game, Graphics g) throws SlickException {
-		map.render(mapX, mapY);
-		
+		// the actual draw position of the map will be determined by the
+		// location of the player on the map
+		map.render(player.getMapX(), player.getMapY());
+
+		// let the player draw itself
 		player.render(con, game, g);
 	}
 	
 	@Override
 	public void update(GameContainer con, StateBasedGame game, int delta) throws SlickException {
 		// first, let the player process any input
-		player.update(con, game, delta);
-		
-		Input input = con.getInput();
-		
-		// move the player (actually the map) if we need to
-		processMovement(input);
+		player.update(con, game, delta, map);
 	}
 
 	@Override
